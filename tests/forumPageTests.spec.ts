63ee5e3512a2e41bae153e1dd18a0176c00e6a45
Smaga Max <smaga.max@gmail.com>
2026-01-31 21:28:02 +0200

    implement test asc created date

--- tests/forumPageTests.spec.ts
+++ tests/forumPageTests.spec.ts
@@ -26,4 +26,19 @@ test.describe("Forum page tests", () => {
 
     expect(date3.getTime()).toBeLessThan(date4.getTime());
   });
+
+  test("should sort asc created date", async () => {
+    expect(homePage.getForumButton()).toBeVisible();
+    await homePage.getForumButton().click();
+    await forumPage.getDiscussionForum().click();
+
+    const createdCells = forumPage.getCreatedCells();
+    const createdText3 = await createdCells.nth(2).innerText();
+    const createdText4 = await createdCells.nth(3).innerText();
+
+    const date3 = new Date(createdText3);
+    const date4 = new Date(createdText4);
+
+    expect(date3.getTime()).toBeGreaterThan(date4.getTime());
+  });
 });
